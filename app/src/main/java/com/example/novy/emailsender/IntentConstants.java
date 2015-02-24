118cd25244e0cc5f8d93a63b2117749010f1daee
novy154 <slnowak154@gmail.com>
2015-02-24 20:05:40 +0100

    Added login-like activity

---
+++ app/src/main/java/com/example/novy/emailsender/IntentConstants.java
@@ -0,0 +1,13 @@
+package com.example.novy.emailsender;
+
+/**
+ * Created by novy on 24.02.15.
+ */
+public class IntentConstants {
+
+    public static final String SENDER_EMAIL_STRING = "senderEmailString";
+    public static final String SENDER_PASSWORD_STRING = "senderPasswordString";
+
+    private IntentConstants() {
+    }
+}
--- app/src/main/java/com/example/novy/emailsender/MainActivity.java
+++ app/src/main/java/com/example/novy/emailsender/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.novy.emailsender;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import org.apache.commons.validator.routines.EmailValidator;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +41,52 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login(View view) {
+        final EditText emailField = (EditText) findViewById(R.id.emailField);
+        final EditText passwordField = (EditText) findViewById(R.id.passwordField);
+
+        final String emailString = emailField.getText().toString();
+        final String passwordString = passwordField.getText().toString();
+
+        if (emailIsValid(emailString) && passwordNotEmpty(passwordString)) {
+            showEmailContentActivity(emailString, passwordString);
+        } else {
+            showErrorDialog();
+        }
+    }
+
+    private void showEmailContentActivity(String emailString, String passwordString) {
+
+        Intent intent = new Intent(MainActivity.this, EmailContentActivity.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString(IntentConstants.SENDER_EMAIL_STRING, emailString);
+        bundle.putString(IntentConstants.SENDER_PASSWORD_STRING, passwordString);
+
+        intent.putExtras(bundle);
+        startActivity(intent);
+        finish();
+    }
+
+    private boolean passwordNotEmpty(String passwordString) {
+        return !passwordString.isEmpty();
+    }
+
+    private void showErrorDialog() {
+        AlertDialog.Builder dialogAlert = new AlertDialog.Builder(this);
+
+        dialogAlert.setMessage("Wrong email format!");
+        dialogAlert.setTitle("Error");
+
+        dialogAlert.setPositiveButton("OK", null);
+        dialogAlert.setCancelable(true);
+        dialogAlert.create().show();
+    }
+
+    private boolean emailIsValid(String emailString) {
+        final EmailValidator emailValidator = EmailValidator.getInstance();
+        return emailValidator.isValid(emailString);
     }
 }
